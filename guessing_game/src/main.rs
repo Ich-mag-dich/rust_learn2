0de9fb01ba52a33091d94e40c35dbe49a982167f
Ich-mag-dich <san02@kakao.com>
2023-03-15 21:31:56 +0900

    quit

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,23 +11,25 @@ fn main() {
     // println!("the secret number is {secret_number}");
 
     loop {
-        println!("\nplease input your guess");
+        println!("\nplease input your guess:");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("failed to read_line");
+            .expect("!!! failed to read_line");
 
-        println!("your guess number is: {guess}");
         // 어떠한 변수의 값을 출력할때라면 중괄호 안에 바로 집어넣고 ex) {guess}
         // 어떠한 표현식의 결과값을 출력할때에는 빈 중괄호와 문자열 뒤 콤마로 출력. ex) "{}", guess + 2
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+            Ok(num) => {
+                println!("your guess number is: {guess}");
+                num
+            }
             Err(_) => {
-                if guess.trim() == String::from("quit").trim() {
+                if guess.trim() == "quit".trim() {
                     break;
                 }
-                println!("please type a number");
+                println!("!!! please type a number");
                 continue;
             }
         };
