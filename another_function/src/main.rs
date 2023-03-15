241c89af9a2b7fc4651486874cc5e1a4eb5b9b52
Ich-mag-dich <san02@kakao.com>
2023-03-15 22:09:05 +0900

    another function

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -1,8 +1,22 @@
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
-    println!("this is {}", another_function());
+    println!("the number is {}", another_function());
 }
 
-fn another_function() -> String {
-    return String::from("another_function");
+fn another_function() -> i32 {
+    println!("input integer:");
+    let mut input_num = String::new();
+    std::io
+        ::stdin()
+        .read_line(&mut input_num)
+        .expect("cannot read line");
+    let input_num: i32 = match input_num.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("this is not integer: {input_num}");
+            exit(0)
+        }
+    };
+    return input_num;
 }
