58efd8695441551e65479b61a73c0c7008242de7
Ich-mag-dich <san02@kakao.com>
2023-03-17 20:49:21 +0900

    learned vectors

---
+++ learn_vectors/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    // let v: Vec<i32> = Vec::new();
+    // let v = vec![1, 2, 3];
+
+    // let mut v = Vec::new();
+
+    // v.push(5);
+    // v.push(6);
+    // v.push(7);
+    // v.push(8);
+
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2]; //index ㅁ무ㄴ법
+    println!("third element is {}", third);
+
+    let third: Option<&i32> = v.get(2); //get method
+    match third {
+        Some(third) => println!("third of element is {third}."),
+        None => println!("there is no third element."),
+    }
+
+    for i in &v {
+        println!("{i}");
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+
+    // --------------------------
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(10.12),
+        SpreadsheetCell::Text(String::from("blue")),
+    ];
+}
