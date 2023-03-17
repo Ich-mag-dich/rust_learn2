fd9640ce55f165123392870e7a80e6ef5ee7943b
Ich-mag-dich <san02@kakao.com>
2023-03-17 15:34:35 +0900

    learned enums

--- learn_enum/src/main.rs
+++ learn_enum/src/main.rs
@@ -26,33 +26,66 @@
 // }
 
 // match ---------------------------------------------------------------------
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    ALaska,
-    // --snip--
-}
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
+// #[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     ALaska,
+//     // --snip--
+// }
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
+// fn main() {
+//     let coin = Coin::Quarter(UsState::Alabama);
+//     println!("hey, {}", value_in_cents(coin));
+// }
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => {
+//             println!("lucky penny");
+//             1
+//         }
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}!", state);
+//             25
+//         }
+//     }
+// } // TODO https://rust-kr.github.io/doc.rust-kr.org/ch06-02-match.html , 6.2장 하다 말았음 .
+
+// Option<t> -------------------------------------------
+
+// fn main() {
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+//     println!("{:?}, {:?}", six, none);
+
+//     let dice_roll = 9;
+//     match dice_roll {
+//         3 => add_fancy_hat(),
+//         7 => remove_fancy_hat(),
+//         _ => (), // 아무것도 안함
+//     }
+
+//     fn add_fancy_hat() {}
+//     fn remove_fancy_hat() {}
+// }
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(x) => Some(x + 1),
+//     }
+// }
+
+// if let --------------------------------------------------------------------------
 fn main() {
-    let coin = Coin::Quarter(UsState::Alabama);
-    println!("hey, {}", value_in_cents(coin));
-}
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("lucky penny");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("max: {}", max);
     }
-} // TODO https://rust-kr.github.io/doc.rust-kr.org/ch06-02-match.html , 6.2장 하다 말았음 .
+}
