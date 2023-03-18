30a1b28e42a802859d77acd6f6b7eff3ef91435c
Ich-mag-dich <san02@kakao.com>
2023-03-18 17:45:55 +0900

    learned Strings

--- learn_strings/src/main.rs
+++ learn_strings/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    println!("Hello, world");
-    println!("asd")
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {s1}, s2 is {s2}");
+
+    s1 = String::from("lo");
+    s1.push('l');
+    println!("s1 is {s1}");
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    println!("{}", s1 + &s2); // note s1 has been moved here and can no longer be used
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{s}");
 }
 
 // TODO 8.2장 부터 해야됨.
