b46334276d3db5c30b043c2a2e91fdb188e65ae7
Ich-mag-dich <san02@kakao.com>
2023-03-20 11:07:21 +0900

    hello.txt

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,8 +2,8 @@ mod my_functions;
 
 pub use my_functions::my_function;
 fn main() {
-    my_function::my_print("qweqwe");
+    my_function::my_print("qwe qwe");
     let file_name = String::from("hello.txt");
     let contents = my_function::text_read(&file_name).expect("can't read file");
-    println!("contents:\n{}", contents);
+    println!("contents: {{\n{}\n}}", contents);
 }
