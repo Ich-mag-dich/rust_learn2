ebbcd7723ee5e226ec2c6134cb6a570f2034e423
Ich-mag-dich <san02@kakao.com>
2023-03-28 12:51:31 +0900

    commit

--- learn_vectors/src/main.rs
+++ learn_vectors/src/main.rs
@@ -1,3 +1,6 @@
+// use core::fmt;
+// use std::{any::Any, fmt::Display};
+
 fn main() {
     // let v: Vec<i32> = Vec::new();
     // let v = vec![1, 2, 3];
@@ -9,25 +12,25 @@ fn main() {
     // v.push(7);
     // v.push(8);
 
-    let v = vec![1, 2, 3, 4, 5];
-    let third: &i32 = &v[2]; //index ㅁ무ㄴ법
-    println!("third element is {}", third);
+    // let v = vec![1, 2, 3, 4, 5];
+    // let third: &i32 = &v[2]; //index ㅁ무ㄴ법
+    // println!("third element is {}", third);
 
-    let third: Option<&i32> = v.get(2); //get method
-    match third {
-        Some(third) => println!("third of element is {third}."),
-        None => println!("there is no third element."),
-    }
+    // let third: Option<&i32> = v.get(2); //get method
+    // match third {
+    //     Some(third) => println!("third of element is {third}."),
+    //     None => println!("there is no third element."),
+    // }
 
-    for i in &v {
-        println!("{i}");
-    }
+    // for i in &v {
+    //     println!("{i}");
+    // }
 
-    let mut v = vec![100, 32, 57];
-    for i in &mut v {
-        *i += 50;
-        println!("{i}");
-    }
+    // let mut v = vec![100, 32, 57];
+    // for i in &mut v {
+    //     *i += 50;
+    //     println!("{i}");
+    // }
 
     // --------------------------
 
@@ -39,6 +42,14 @@ fn main() {
     let row = vec![
         SpreadsheetCell::Int(3),
         SpreadsheetCell::Float(10.12),
-        SpreadsheetCell::Text(String::from("blue"))
+        SpreadsheetCell::Text(String::from("blue")),
     ];
+
+    for i in &row {
+        match i {
+            SpreadsheetCell::Int(i) => println!("Int: {i}"),
+            SpreadsheetCell::Float(i) => println!("Float: {i}"),
+            SpreadsheetCell::Text(i) => println!("Text: {i}"),
+        }
+    }
 }
